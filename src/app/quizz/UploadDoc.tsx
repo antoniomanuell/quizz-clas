a33bc45b06e67bced256e876d202124de727cda3
antoniomanuell <antoniomanuel.iot@outlook.com>
2025-06-16 00:07:26 +0100

    OpenAI Integration

--- src/app/quizz/UploadDoc.tsx
+++ src/app/quizz/UploadDoc.tsx
@@ -5,11 +5,30 @@ import { Button } from "@/components/ui/button";
 const UploadDoc = () => {
     const [document, setDocument] = useState<Blob | File | null | undefined >(null);
     const [isLoading, setIsLoading] = useState<boolean>(false); 
+    const [error, setError] = useState<string>("");
+
 
     const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
+        if (!document) {
+            setError("Por favor, carregue o documento primeiro");
+            return;
+        } 
         setIsLoading(true);
-        console.log(document);
+        const formData = new FormData();
+        formData.append("pdf", document as Blob);
+        try {
+            const res = await fetch("/api/quizz/generate", {
+                method: "POST",
+                body: formData
+            });
+            if (res.status === 200) {
+                console.log("quizz generated successfully");
+            }
+        } catch (e) {
+            console.log ("error while generating", e);
+        }
+        setIsLoading(false); 
     }
 
     return (
@@ -21,6 +40,7 @@ const UploadDoc = () => {
                     <div className="absolute inset-0 m-auto flex justify-center items-center">{document && document?.name ? document.name : "Enviar um PDF"}</div>
                 <input type="file" id="document" className="relative block w-full h-full z-50 opacity-0" onChange={(e) => setDocument (e?.target?.files?.[0])}/>
                 </label>
+                {error ? <p className="text-red-600">{error}</p> : null}
                 <Button size="lg" className="mt-2"
                 type="submit">Gerar Quiz</Button>
             </form>
--- src/app/quizz/new/page.tsx
+++ src/app/quizz/new/page.tsx
@@ -4,7 +4,7 @@ const page = () => {
     return (
         <div className="flex flex-col flex-1">
             <main className="py-11 flex flex-col text-center gap-4 items-center flex-1 mt-24">
-                <h2 className="text-3xl font-bold mb-4">What do you want to be quizzed about today?</h2>
+                <h2 className="text-3xl font-bold mb-4">Vai testar sua memória com que quiz hoje?</h2>
                 <UploadDoc />
             </main>
         </div>
